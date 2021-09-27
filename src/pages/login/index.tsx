bb303f57758ebe1a1da3a2a609789a64cf47288e
vewevteen <vewevteen0414@gmail.com>
2021-09-27 17:29:38 +0900

    react hook form 로그인 회원가입 페이지 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,21 @@
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
 const Login: React.FC = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data: { id: string, password: string }) => {
+        console.log(data);
+    }
     return (
-        <div>로그인 페이지</div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>로그인 페이지</div>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <input placeholder="아아디" style={{ width: '150px' }} {...register("id")} />
+                <input type="password" placeholder="비밀번호" style={{ width: '150px' }} {...register("password")} />
+            </div>
+            <button type="submit">로그인</button>
+            <Link href="/signup" passHref><button>회원가입</button></Link>
+        </form>
     )
 }
 
