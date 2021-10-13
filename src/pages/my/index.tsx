1b5d3ec588a217be9ca3ed689910269428a00b94
vewevteen <vewevteen0414@gmail.com>
2021-10-13 11:09:25 +0900

    스타일 추가

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -16,24 +16,34 @@ const Profile = styled.div`
   background-color: #c4c4c4;
 `;
 
+const BlueUnderLine = styled.div`
+  color: #007aff;
+  text-decoration: underline #007aff;
+`;
+
 const My: React.FC = () => {
   return (
-    <Wrapper style={{ justifyContent: 'center' }}>
-      <Flex flexDirection="column">
-        <Spacer size={80} />
+    <Wrapper
+      style={{
+        justifyContent: 'center',
+        paddingTop: '80px',
+        paddingBottom: '80px',
+      }}
+    >
+      <Flex flexDirection="column" width={['', '372px']}>
         <Flex flexDirection="column" alignItems="center">
           <Profile />
-          <Text fontSize="17px" fontWeight="600" mt="24px">
+          <Text fontSize="17px" fontWeight="bold" mt="24px" mb="10px">
             Your Id
           </Text>
-          <div>프로필 사진 바꾸기</div>
+          <BlueUnderLine>프로필 사진 바꾸기</BlueUnderLine>
         </Flex>
         <Spacer size={48} />
         <Stack gap={20}>
           <Stack>
             <Flex sx={{ py: '8px' }}>
               <Label>이메일</Label>
-              <Text style={{ flex: 1, marginLeft: '30px' }}>
+              <Text style={{ flex: 1, marginLeft: '34px' }}>
                 lckdodive@battleent.com
               </Text>
             </Flex>
@@ -41,13 +51,13 @@ const My: React.FC = () => {
           <Stack>
             <Flex alignItems="center">
               <Label>닉네임</Label>
-              <Input style={{ flex: 1, marginLeft: '30px' }} />
+              <Input style={{ flex: 1, marginLeft: '34px' }} />
             </Flex>
           </Stack>
           <Stack>
             <Flex alignItems="center">
               <Label>언어</Label>
-              <Select style={{ flex: 1, marginLeft: '43px' }}>
+              <Select style={{ flex: 1, marginLeft: '46px' }}>
                 <option>한글</option>
                 <option>English</option>
                 <option>中國語</option>
@@ -57,7 +67,7 @@ const My: React.FC = () => {
           <Stack>
             <Flex alignItems="center">
               <Label>국가</Label>
-              <Select style={{ flex: 1, marginLeft: '43px' }}>
+              <Select style={{ flex: 1, marginLeft: '46px' }}>
                 <option>한국</option>
                 <option>US</option>
                 <option>CHINA</option>
@@ -70,6 +80,7 @@ const My: React.FC = () => {
               marginLeft: 'auto',
               marginTop: '48px',
               marginBottom: '40px',
+              padding: '12px 18px',
             }}
           >
             저장
@@ -84,15 +95,27 @@ const My: React.FC = () => {
             borderBottom: '1px solid #EBEBEB',
           }}
         >
-          <Text fontWeight="700" fontSize="18px" mb="12px">
+          <Text color="#343434" fontSize="18px" fontWeight="bold" mb="12px">
             Wallet
           </Text>
           <Flex>
-            <Label style={{ marginRight: '48px' }}>내 지갑 주소</Label>
-            <Text>0xe3d92072d8b9a59a0427485a1b5f459271df</Text>
+            <Label style={{ marginRight: '48px', whiteSpace: 'nowrap' }}>
+              내 지갑 주소
+            </Label>
+            <Text
+              style={{ wordBreak: 'break-all', textDecoration: 'underline' }}
+            >
+              0xe3d92072d8b9a59a0427485a1b5f459271df
+            </Text>
           </Flex>
         </Flex>
-        <Flex flexDirection="column" py="40px">
+        <Flex
+          flexDirection="column"
+          py="40px"
+          sx={{
+            borderBottom: '1px solid #EBEBEB',
+          }}
+        >
           <Text color="#343434" fontSize="18px" fontWeight="bold">
             비밀번호 변경
           </Text>
@@ -128,8 +151,13 @@ const My: React.FC = () => {
           </Stack>
           <Spacer size={40} />
           <Flex justifyContent="flex-end">
-            <Button type="submit">비밀번호 변경</Button>
+            <Button type="submit" style={{ padding: '12px 18px' }}>
+              비밀번호 변경
+            </Button>
           </Flex>
+        </Flex>
+        <Flex justifyContent="center" mt="32px">
+          <BlueUnderLine>로그아웃</BlueUnderLine>
         </Flex>
       </Flex>
     </Wrapper>
