81d1c30a8efb2bd06cff83afd3c147b3cf28d3a6
vewevteen <vewevteen0414@gmail.com>
2021-11-24 17:45:40 +0900

    delete comment

--- src/modules/apolloClient.ts
+++ src/modules/apolloClient.ts
@@ -5,9 +5,7 @@ import { setContext } from '@apollo/client/link/context';
 const restLink = new RestLink({ uri: 'https://api-platform.klet.ninja/' });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('access_token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
