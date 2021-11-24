a080519fa00de55e0565bf68bc76b32b059f2051
안도현 <doh9898@gmail.com>
2021-11-24 17:40:12 +0900

    Do not use getInitialProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import { useLayoutEffect } from 'react';
 import { ApolloProvider } from '@apollo/client';
+import { useRouter } from 'next/router';
 
 import Client from '@/modules/apolloClient';
 import type { AppPropsWithLayout } from '@/layouts/types';
@@ -7,8 +9,20 @@ import DefaultLayout from '@/layouts/DefaultLayout';
 import GlobalStyle from '@/styles/GlobalStyle';
 
 const App: React.FC<AppPropsWithLayout> = ({ Component, pageProps }) => {
+  const router = useRouter();
   const getLayout =
     Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
+
+  useLayoutEffect(() => {
+    const accessToken =
+      typeof window !== 'undefined'
+        ? localStorage.getItem('access_token')
+        : null;
+
+    if (!accessToken) {
+      router.push('/login');
+    }
+  }, [router]);
 
   return (
     <ApolloProvider client={Client}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,26 +2,14 @@ import { useEffect } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
-const Home: NextPage<{ accessToken: string | null }> = ({ accessToken }) => {
+const Home: NextPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    if (accessToken) {
-      router.replace('/nft');
-    }
-
-    router.push('/login');
-  }, [accessToken, router]);
+    router.replace('/nft');
+  }, [router]);
 
   return null;
-};
-
-Home.getInitialProps = async () => {
-  const accessToken =
-    typeof window !== 'undefined' ? localStorage.getItem('access_token') : null;
-  return {
-    accessToken: accessToken,
-  };
 };
 
 export default Home;
