a5f1dd56b32ff55abae1b3686a39fd5e5a73edfd
안도현 <doh9898@gmail.com>
2021-11-25 12:50:29 +0900

    Don't use getInitialProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,5 @@
-import { useEffect } from 'react';
-import { NextPage } from 'next';
-import { useRouter } from 'next/router';
-
-const Home: NextPage<{ accessToken: string | null }> = ({ accessToken }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (accessToken) {
-      router.replace('/nft');
-    }
-
-    router.push('/login');
-  }, [accessToken, router]);
-
+const Home = () => {
   return null;
-};
-
-Home.getInitialProps = async () => {
-  const accessToken =
-    typeof window !== 'undefined' ? localStorage.getItem('access_token') : null;
-  return {
-    accessToken: accessToken,
-  };
 };
 
 export default Home;
